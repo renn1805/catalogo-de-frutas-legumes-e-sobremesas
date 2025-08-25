747609d1f99d8fdbc81e1649f1a9711c251dbbb2
renn1805 <renan.almeida.arau@gmail.com>
2025-08-25 18:07:46 -0300

    first commit

---
+++ src/frutas.js
@@ -0,0 +1,8 @@
+// const flipCards = document.querySelectorAll('.flipCard')
+
+// flipCards.forEach(card => {
+//     card.addEventListener('click', () => {
+//         card.classList.toggle('flip')
+//         console.log(card)
+//     })
+// })
