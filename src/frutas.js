43f96d24cc96eb01f582a4ef06bd1af442672af9
renn1805 <renan.almeida.arau@gmail.com>
2025-08-30 19:43:24 -0300

    refatoração e adição de comentarios as paginas de css

--- src/frutas.js
+++
@@ -1,8 +0,0 @@
-// const flipCards = document.querySelectorAll('.flipCard')
-
-// flipCards.forEach(card => {
-//     card.addEventListener('click', () => {
-//         card.classList.toggle('flip')
-//         console.log(card)
-//     })
-// })
